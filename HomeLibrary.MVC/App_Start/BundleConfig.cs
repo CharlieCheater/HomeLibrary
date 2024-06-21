24972bf5907eb0e319baf8dbd877520d75383297
Charlie Cheater <charliecheaters@yandex.ru>
2024-06-21 14:14:51 +0300

    Добавил js библиотеку для редактирования XML данных

--- HomeLibrary.MVC/App_Start/BundleConfig.cs
+++ HomeLibrary.MVC/App_Start/BundleConfig.cs
@@ -19,12 +19,19 @@ namespace HomeLibrary.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
+
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/codemirror").Include(
+            "~/Scripts/codemirror*",
+            "~/Scripts/xml*"));
+            bundles.Add(new StyleBundle("~/Content/codemirror").Include(
+                      "~/Content/codemirror.css"));
         }
     }
 }
